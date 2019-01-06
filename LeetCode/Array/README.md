e7a07329607507f42a99bd4fa67c3a46795569af
xiaojkql <xiaojkql@163.com>
2019-01-06 18:13:23 +0800

    Update

--- LeetCode/Array/README.md
+++ LeetCode/Array/README.md
@@ -4,4 +4,5 @@
 
 1. Two sum [Web](https://leetcode.com/problems/two-sum/) [code]()
 2. [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/) [code]()
-
+3. [Remove Element](https://leetcode.com/problems/remove-element)
+4. [Maximum Subarray](https://leetcode.com/problems/maximum-subarray/)  tutorial [1](https://leetcode.com/problems/maximum-subarray/discuss/20193/DP-solution-and-some-thoughts)，[2](https://leetcode.com/problems/maximum-subarray/discuss/20452/C%2B%2B-DP-and-Divide-and-Conquer)，[3](https://leetcode.com/problems/maximum-subarray/discuss/118509/C++-Kadane's-Algorithm)，[DC](https://www.geeksforgeeks.org/maximum-subarray-sum-using-divide-and-conquer-algorithm/)
---
+++ LeetCode/Array/easy_merge_sorted_array.py
@@ -0,0 +1,44 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        # idea ： 将最大的排在最后面，然后依次排
+        k = m + n - 1
+        i = m - 1  # 第一个数组的最后一个元素
+        j = n - 1  # 第二个数组的最后一个元素
+        while i >=0 and j >=0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+        while j>=0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
+
+    def merge2(self,nums1,m,nums2,n):
+        while n > 0 and m > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[n + m - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[n + m - 1] = nums2[n - 1]
+                n -= 1
+        if n > 0:
+            nums1[0:n] = nums2[0:n]
+
+if __name__ == "__main__":
+    nums1 = [1,2,7,10,56,0,0,0,0,0,0,0]
+    nums2 = [2,3,8,9,11,100]
+    solution = Solution()
+    solution.merge(nums1,5,nums2,6)
+    print(nums1)
