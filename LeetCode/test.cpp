643823a46cb0801048ecd7afd305b315da84bb57
xiaojkql <xiaojkql@163.com>
2019-01-16 14:53:51 +0800

    cpp

---
+++ LeetCode/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 10;
+    int b = 20;
+    cout << a + b << endl;
+    system("pause");
+    return 0;
+}
