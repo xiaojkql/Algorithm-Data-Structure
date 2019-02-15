1ac0d8e459071b33459ceb59160ed983efdf1eb1
xiaojkql <xiaojkql@163.com>
2019-02-15 22:20:54 +0800

    quickSort

--- Basic Data Structure/Sort and Search/Sort/quickSort.py
+++ Basic Data Structure/Sort and Search/Sort/quickSort.py
@@ -12,12 +12,14 @@ Time: 2019-02-15 21:15:43
 def quickSort(ls, lo, hi):  # [lo, hi)
     if hi - lo < 2:  # 递归基，单元素就不需要再进行划分
         return
-    mi = partition(ls, lo, hi-1)  # 找轴点
+    mi = partitionB(ls, lo, hi-1)  # 找轴点
     quickSort(ls, lo, mi)  # 对轴点前面的部分进行快排
     quickSort(ls, mi+1, hi)  # 对轴点后面的部分进行快排
 
 
-def partition(ls, lo, hi):
+def partitionA(ls, lo, hi):  # 对区间[lo, hi]区间中的列表元素进行划分
+    """此版本是将小于等于候选pivot归入左边
+    大于等于候选pivot的归入右边"""
     pivot = ls[lo]
     while lo < hi:
         while (lo < hi) and (pivot <= ls[hi]):
@@ -26,6 +28,29 @@ def partition(ls, lo, hi):
         while (lo < hi) and (ls[lo] <= pivot):
             lo += 1
         ls[hi] = ls[lo]
+    ls[lo] = pivot
+    return lo
+
+
+def partitionB(ls, lo, hi):  # 对[lo, hi]区间中的列表元素进行划分
+    """此版本是将大于候选pivot的归入右边
+    将小于候选pivot的归入左边"""
+    pivot = ls[lo]  # 将首元素作为候选轴点
+    while lo < hi:
+        while lo < hi:
+            if pivot < ls[hi]:
+                hi -= 1
+            else:
+                ls[lo] = ls[hi]
+                lo += 1
+                break
+        while lo < hi:
+            if ls[lo] < pivot:
+                lo += 1
+            else:
+                ls[hi] = ls[lo]
+                hi -= 1
+                break
     ls[lo] = pivot
     return lo
 
